27a6ac66b2de1292a604b32248ffd25f87d07f4f
Sakah Cedric <sakahcedric50@gmail.com>
2025-12-02 17:23:32 +0100

    setting up the database connection with mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import cors from "cors";
+import { connect } from "bun";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+import mongoose from "mongoose";
 
+mongoose
+  .connect("mongodb://localhost:27017/skill-Serve-db")
+  .then(() => {
+    console.log("✅ MongoDB Connected Successfully");
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB Connection Failed:", err);
+  });
 
 // Allow requests from Vite frontend
 app.use(
